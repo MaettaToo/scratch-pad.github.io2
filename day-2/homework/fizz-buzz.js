dc9648155ca31b67916eeca069a32696f80f82ac
MaettaToo <maetta.green2@gmail.com>
2025-03-19 03:36:54 +0000

    completed fizzbuzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -12,7 +12,30 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
+    //I: factory function that takes in numbers 1-100
+    //O: prints numbers 1-100, for multiples of 3 fizz prints, multiples of five buzz prints, multiples of 3 & 5 FizzBuzz prints
+    //C: none
+    //E: none
+    //init loop that counts between 1 and 100
+    for(var i = 1; i <= 100; i++){
+    //create conditional statements to print 1-100
+        if(i % 3 !== 0 && i % 5 !== 0){
+            console.log(i);
+        }
+        // create independent conditional stmts for multiples of three print “Fizz”
+         if(i % 3 === 0 && i % 5 !== 0){ 
+            console.log('Fizz');
+        }
+        // create independent conditional stmts for multiples of five print “Buzz”
+         if(i % 5 === 0 && i % 3 !== 0){ 
+            console.log('Buzz');
+        }
+        // create independent conditional stmts for multiples of three and five print “FizzBuzz”
+        if(i % 5 === 0 && i % 3 === 0){ 
+                        console.log('FizzBuzz');
+        }
+    
+    } 
 
 
     // YOUR CODE GOES ABOVE HERE //
